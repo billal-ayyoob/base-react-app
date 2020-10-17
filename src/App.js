5b39443f2dcc3b43d5053ae1faee7560767cb80b
Bilal Ayyoob <mbilalayyoob@gmail.com>
2020-10-18 04:14:21 +0500

    React app initialization

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class MyComponent extends React.Component{
+	render(){
+		return(
+			<React.Fragment>
+				<h1>Hello from VLT team</h1>
+			</React.Fragment>
+		)	
+	}
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyComponent/>
   );
 }
 
