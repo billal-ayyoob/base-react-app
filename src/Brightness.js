0e06d31832eeff37350e088289d7a024662795d4
Bilal Ayyoob <mbilalayyoob@gmail.com>
2020-10-21 05:28:38 +0500

    Update Brightness component

--- src/Brightness.js
+++ src/Brightness.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function Brightness(props) {
     return (
         <div>
-            <h1>The room {props.roomNum} is {props.brightness ? 'lit' : 'dark'} at floor {props.currentFloor}</h1>
+            <h1>The room <span className="temp">{props.roomNum}</span> is {props.brightness ? 'lit' : 'dark'} at floor <span className="temp">{props.currentFloor}</span></h1>
         </div>
     );
 }
